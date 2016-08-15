99545f0ee718867c0c33de225e11a495dbc6a934
kuzovkov aleksandr <kuzovkov.aleksandr@rambler.ru>
2016-08-15 23:51:57 +0300

    добавил расчет профита в таблицу статистики по балансам

--- web/scripts/get_balances.php
+++ web/scripts/get_balances.php
@@ -23,13 +23,17 @@ $data = get_balances_data($fullname);
             <td>&nbsp;
             <h3> Сессия: <?php echo $session_id;?> </h3>
             <?php foreach ($session_data as $currency => $items):?>
+                <?php $first_amount = (count($items))? $items[0]['amount'] : 1; $prev_amount = $first_amount;?>
                 <table class="table-bordered">
-                <tr><th colspan="2"><h4> Валюта: <?php echo $currency;?></h4></th></tr>
-                    <tr><td> Дата </td><td> Сумма </td></tr>
+                <tr><th colspan="4"><h4> Валюта: <?php echo $currency;?></h4></th></tr>
+                    <tr><td> Дата </td><td> Сумма </td><td>Профит(%)</td><td>Итого(%)</td></tr>
                 <?php foreach($items as $item):?>
                     <tr>
                         <td> <?php echo date('d.m.Y H:i:s', $item['utime']);?> </td>
                         <td> <?php echo $item['amount'];?> </td>
+                        <td> <?php echo ($prev_amount != 0)? ($item['amount'] - $prev_amount)/$prev_amount * 100.0 : '-';?> </td>
+                        <td> <?php echo ($first_amount != 0)? ($item['amount'] - $first_amount)/$first_amount * 100.0 : '-';?> </td>
+                        <?php $prev_amount = $item['amount']?>
                     </tr>
                 <?php endforeach;?>
                 </table>
